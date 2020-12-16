5e88259652e52be383f598c5aafa6891dd34fce1
rockplanete <47388339+rockplanete@users.noreply.github.com>
2020-12-16 10:29:24 +0100

    Update Projet2Fonctions.R

--- R/Projet2Fonctions.R
+++ R/Projet2Fonctions.R
@@ -99,7 +99,7 @@ supprBloc <- function(hauteur,largeur,mat){                                    #
 ################################################################################
 
 #Supprime les détails sur les bords droit et gauche ############################
-detail <- function(hauteur,mat){                                               #
+detail <- function(hauteur,largeur,mat){                                               #
   l<-1
   boucle<-0
   while(boucle<2){
@@ -208,7 +208,7 @@ OneSimu <- function(largeur,hauteur,evolve){                                   #
     co<-turn101to111(hauteur,largeur,co)
     co<-supprBloc(hauteur,largeur,co)
     co<-turn101to111(hauteur,largeur,co)
-    co<-detail(hauteur,co)
+    co<-detail(hauteur,largeur,co)
     FinalTab<-rbind(FinalTab,counter(alea,co,hauteur,largeur))
     i<-i+1
   }
